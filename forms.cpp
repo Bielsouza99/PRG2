44895f14e3d51f5208b097e8640a120c1257a290
Bielsouza99 <gabrielespindolass@gmail.com>
2020-02-21 11:11:40 -0300

    Update forms.cpp

--- forms.cpp
+++ forms.cpp
@@ -13,7 +13,7 @@ string substitui(string texto, string sub, string outra){
 }
 
 int main(int argc, char * argv[]) {
-    string dado;
+    string dado, linha, cidade, data, aluno, disciplina;
     string arquivo = argv[1];
 
     fstream arq(arquivo);
@@ -24,8 +24,30 @@ int main(int argc, char * argv[]) {
         for(int i = 2; i < argc; i++){
             dado = argv[i];
             int pos = dado.find("=");
-            dado = dado.erase(0,pos+1);
-            cout << dado << endl;
+            auto formData = dado.substr(0,pos);
+            //cout << formData << endl;
+            if(formData == "cidade"){
+                cidade = dado.erase(0,pos+1);
+            }else if(formData == "data"){
+                data = dado.erase(0,pos+1);
+            } else if(formData == "aluno") {
+                aluno = dado.erase(0,pos+1);
+            } else if(formData == "disciplina"){
+                disciplina = dado.erase(0,pos+1);
+            }else {
+                cout << "Não foi achado nenhum parâmetro correspondente" << endl;
+            }
+            // dado = dado.erase(0,pos+1);
         }
+
+        cout << cidade << endl << data << endl << aluno << endl << disciplina << endl;
+        // while(arq >> linha){
+        //     if(linha.size() > 0){
+        //         int pos = linha.find("$");
+        //         auto var = linha.substr(pos+1,linha.size());
+        //         auto newP = substitui(linha, var, "");
+        //         cout << newP << endl;
+        //     }
+        // }
     }
 }
